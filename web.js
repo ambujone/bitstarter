2741b1fb54b48324b19a5c633203da6715a6c223
Ambuj Agrawal <ambujone@gmail.com>
2013-07-10 01:08:32 -0700

    testing 1

--- web.js
+++ web.js
@@ -2,9 +2,17 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Testing and debugging');
+  response.send(index);
 });
+
+
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
